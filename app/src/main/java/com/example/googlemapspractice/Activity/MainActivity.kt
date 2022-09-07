a0ee3c7014f51155e7ed7c83101f322305f685fc
Kelvin Josué Estrada López <kestradalp@gmail.com>
2022-09-07 11:33:54 -0500

    ubication db

--- app/src/main/java/com/example/googlemapspractice/Activity/MainActivity.kt
+++ app/src/main/java/com/example/googlemapspractice/Activity/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity :
     OnMapReadyCallback {
 
     lateinit var mMap: GoogleMap
-    val url = "https://my-json-server.typicode.com/kestradalp/GoogleMapsPractice/JsonAPIs/markersUteq"
+    val url = "/kestradalp/GoogleMapsPractice/JsonAPIs/markersUteq"
     var markerList: List<MarkerFaculty> = java.util.ArrayList<MarkerFaculty>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
